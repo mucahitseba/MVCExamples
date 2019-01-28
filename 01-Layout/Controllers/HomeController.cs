b6c6723874bcbe22982f710ac81ea06c8c8e05e9
mucahitseba <mucahitseba@gmail.com>
2019-01-28 12:57:57 +0300

    Add project files.

---
+++ 01-Layout/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult İletisim()
+        {
+            return View();
+        }
+
+    }
+}
